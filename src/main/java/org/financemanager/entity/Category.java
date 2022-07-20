3c529bb64ba47aebcfd766323b19df84324c637c
vasyldmitrovich <vasyl.dmitrovich@gmail.com>
2022-07-20 16:19:35 +0300

    Add needed method and delete unneeded lines

--- src/main/java/org/financemanager/entity/Category.java
+++ src/main/java/org/financemanager/entity/Category.java
@@ -1,17 +1,15 @@
 package org.financemanager.entity;
 
 import javax.persistence.*;
+import java.util.Objects;
 
 @Entity
 @Table(name = "categories")
-
 public class Category {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-    @Column(name = "name")
     private String name;
-    @Column(name = "description")
     private String description;
 
     public Category(){
@@ -45,5 +43,27 @@ public class Category {
 
     public void setDescription(String description) {
         this.description = description;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Category category = (Category) o;
+        return Objects.equals(id, category.id) && Objects.equals(name, category.name) && Objects.equals(description, category.description);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, description);
+    }
+
+    @Override
+    public String toString() {
+        return "Category{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                '}';
     }
 }
