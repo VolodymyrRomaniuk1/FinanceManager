060a12b7053fb632a0ab78c0e6c7b72a419a4da3
vasyldmitrovich <vasyl.dmitrovich@gmail.com>
2022-07-20 16:21:26 +0300

    Add findAll

--- src/main/java/org/financemanager/repository/CategoryRepo.java
+++ src/main/java/org/financemanager/repository/CategoryRepo.java
@@ -3,9 +3,10 @@ package org.financemanager.repository;
 import org.financemanager.entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface CategoryRepo extends JpaRepository<Category, Long> {
     Optional<Category> findByName(String name);
-    //Iterable<Category> findAll();
+    List<Category> findAll();
 }
