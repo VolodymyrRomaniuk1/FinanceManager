4ece37a28321fd503f52ce66576862f277fefb52
Volodymyr Romaniuk <volodyaromanyuk@gmail.com>
2022-07-21 21:18:28 +0300

    Added js for table autogeneration

--- src/main/java/org/financemanager/controller/CategoryController.java
+++ src/main/java/org/financemanager/controller/CategoryController.java
@@ -6,56 +6,35 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.view.RedirectView;
 
 import java.util.List;
 import java.util.Optional;
 
-@Controller
+@RestController
 @RequestMapping("/categories")
 public class CategoryController {
 
     @Autowired
     CategoryRepo categoryRepo;
 
-    @GetMapping
-    public String getCategories(Model model){
-        List<Category> categories = categoryRepo.findAll();
-        model.addAttribute("listCategories", categories);
-        return "categories/categories";
-    }
-
     @PostMapping
-    public String createCategory(@ModelAttribute("category") Category category) {
+    public Category createCategory(@ModelAttribute("category") Category category) {
         categoryRepo.save(category);
-        return "redirect:/categories";
-    }
-
-    @GetMapping("/new")
-    public String newCategory(@ModelAttribute("category") Category category, Model model){
-        return "categories/new";
+        return category;
     }
 
     @PutMapping("{id}")
-    public String updateCategory(@ModelAttribute("category") Category category, @PathVariable("id") Long id){
+    public Category updateCategory(@ModelAttribute("category") Category category, @PathVariable("id") Long id){
         categoryRepo.save(category);
-        return "redirect:/categories";
-    }
-
-    @GetMapping("/{id}/edit")
-    public String editCategory(@PathVariable("id") Long id, Model model){
-        model.addAttribute("category", categoryRepo.getById(id));
-        return "categories/edit";
+        return category;
     }
 
     @DeleteMapping("{id}")
-    public String deleteCategory(@PathVariable("id") Long id){
+    public void deleteCategory(@PathVariable("id") Long id){
         categoryRepo.deleteById(id);
-        return "redirect:/categories";
     }
 
     @GetMapping("{id}")
-    @ResponseBody
     public Optional<Category> getById(@PathVariable("id") Long id){
         Optional<Category> category;
         category = categoryRepo.findById(id);
--- src/main/java/org/financemanager/controller/IndexController.java
+++ src/main/java/org/financemanager/controller/IndexController.java
@@ -1,17 +1,48 @@
 package org.financemanager.controller;
 
-import org.springframework.context.annotation.ComponentScan;
+import org.financemanager.entity.Category;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
+import org.financemanager.repository.CategoryRepo;
+
+import java.util.List;
 
 @Controller
 public class IndexController {
+
+    @Autowired
+    CategoryRepo categoryRepo;
 
     @GetMapping("/")
     public String index (@RequestParam(name = "name", required = false, defaultValue = "World") String name, Model model){
         model.addAttribute("name", name);
         return "index";
+    }
+    @GetMapping("/categories")
+    @ResponseBody
+    public List<Category> getCategoriesList(Model model){
+        List<Category> categories = categoryRepo.findAll();
+        model.addAttribute("listCategories", categories);
+        return categories;
+    }
+
+    @GetMapping("/categoriesList")
+    public String getCategories(Model model){
+        List<Category> categories = categoryRepo.findAll();
+        model.addAttribute("listCategories", categories);
+        return "categories/categoriesList";
+    }
+
+    @GetMapping("categories/{id}/edit")
+    public String editCategory(@PathVariable("id") Long id, Model model){
+        model.addAttribute("category", categoryRepo.getById(id));
+        return "categories/edit";
+    }
+
+    @GetMapping("categories/new")
+    public String newCategory(@ModelAttribute("category") Category category, Model model){
+        return "categories/new";
     }
 }
