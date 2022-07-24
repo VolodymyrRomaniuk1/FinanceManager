1752e048f0da227ce6988165086aac1cf083015c
Volodymyr Romaniuk <volodyaromanyuk@gmail.com>
2022-07-24 18:50:51 +0300

    Added HTTP response codes

--- src/main/java/org/financemanager/controller/CategoryController.java
+++ src/main/java/org/financemanager/controller/CategoryController.java
@@ -3,6 +3,8 @@ package org.financemanager.controller;
 import org.financemanager.entity.Category;
 import org.financemanager.repository.CategoryRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -17,27 +19,47 @@ public class CategoryController {
     @Autowired
     CategoryRepo categoryRepo;
 
-    @PostMapping
-    public Category createCategory(@ModelAttribute("category") Category category) {
-        categoryRepo.save(category);
-        return category;
-    }
-
-    @PutMapping("{id}")
-    public Category updateCategory(@ModelAttribute("category") Category category, @PathVariable("id") Long id){
-        categoryRepo.save(category);
-        return category;
-    }
-
-    @DeleteMapping("{id}")
-    public void deleteCategory(@PathVariable("id") Long id){
-        categoryRepo.deleteById(id);
+    @GetMapping
+    public ResponseEntity<List<Category>> getCategoriesList(Model model){
+        List<Category> categories = categoryRepo.findAll();
+        if(categories.isEmpty()){
+            return new ResponseEntity<>(categories, HttpStatus.NOT_FOUND);
+        }
+        model.addAttribute("listCategories", categories);
+        return new ResponseEntity<>(categories, HttpStatus.OK);
     }
 
     @GetMapping("{id}")
-    public Optional<Category> getById(@PathVariable("id") Long id){
+    public ResponseEntity<Optional<Category>> getById(@PathVariable("id") Long id){
         Optional<Category> category;
         category = categoryRepo.findById(id);
-        return category;
+        if(category.isEmpty()){
+            return new ResponseEntity<>(category, HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(category, HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<Category> createCategory(@ModelAttribute("category") Category category) {
+        if(category == null){
+            return new ResponseEntity<>(category, HttpStatus.NOT_FOUND);
+        }
+        categoryRepo.save(category);
+        return new ResponseEntity<>(category, HttpStatus.CREATED);
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<Category> updateCategory(@ModelAttribute("category") Category category){
+        if(category == null){
+            return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+        }
+        categoryRepo.save(category);
+        return new ResponseEntity<>(null, HttpStatus.OK);
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity deleteCategory(@PathVariable("id") Long id){
+        categoryRepo.deleteById(id);
+        return new ResponseEntity<>(null, HttpStatus.OK);
     }
 }
