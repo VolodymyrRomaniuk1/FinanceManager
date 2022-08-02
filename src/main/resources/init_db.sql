f461ce43ad8d4493a07ae63c9ca2d2c245dc06cd
Volodymyr Romaniuk <volodyaromanyuk@gmail.com>
2022-08-02 20:54:24 +0300

    Updated script to create transactions table

--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -1,4 +1,10 @@
-create table if not exists categories
+create table if not exists categories(
+    id bigserial NOT NULL,
+    name text,
+    description text
+    PRIMARY KEY (id)
+);
+    select setval('categories_id_seq', (select max(id) from public.categories));
     insert into
     categories
 (
@@ -46,3 +52,35 @@ create table if not exists categories
     'categoryName10',
     'Description10'
 );
+
+create table if not exists public.transactions
+(
+    id bigserial NOT NULL,
+    category_id bigint NOT NULL,
+    operation_type text NOT NULL,
+    sum double precision NOT NULL,
+    date date NOT NULL,
+    description text,
+    PRIMARY KEY (id),
+    FOREIGN KEY (category_id) REFERENCES categories (id)
+    );
+
+ALTER TABLE IF EXISTS public.transactions
+    OWNER to postgres,
+    ADD CONSTRAINT check_operation_type
+    CHECK (operation_type = 'Spending' OR operation_type = 'Gain');
+
+
+select setval('transactions_id_seq', (select max(id) from public.transactions));
+insert into
+    transactions
+(
+    operation_type,
+    sum,
+    date,
+    description
+)
+values
+    (
+
+    ),
