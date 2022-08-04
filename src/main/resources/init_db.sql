c93d380b391e164fa1351c780427c9c70c4577c0
Volodymyr Romaniuk <volodyaromanyuk@gmail.com>
2022-08-04 22:16:41 +0300

    Added value insertions for transaction table

--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -75,6 +75,7 @@ select setval('transactions_id_seq', (select max(id) from public.transactions));
 insert into
     transactions
 (
+    category_id,
     operation_type,
     sum,
     date,
@@ -82,5 +83,23 @@ insert into
 )
 values
     (
-
+        4,
+        'Spending',
+        93.0,
+        '2022-08-01',
+        'Description 1'
     ),
+    (
+        1,
+        'Spending',
+        10.0,
+        '2022-08-02',
+        'Description 2'
+    ),
+    (
+        2,
+        'Gain',
+        500.0,
+        '2022-08-03',
+        'Description 3'
+    )
