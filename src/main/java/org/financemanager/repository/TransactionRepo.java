e2c1817cf673b68f14add382e2b72bbee580bd97
Volodymyr Romaniuk <volodyaromanyuk@gmail.com>
2022-08-09 18:43:53 +0300

    Added find by id method

--- src/main/java/org/financemanager/repository/TransactionRepo.java
+++ src/main/java/org/financemanager/repository/TransactionRepo.java
@@ -1,10 +1,16 @@
 package org.financemanager.repository;
 
+import org.financemanager.entity.Category;
 import org.financemanager.entity.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface TransactionRepo extends JpaRepository<Transaction, Long> {
     List<Transaction> findAll();
+
+    Optional<Transaction> findById(Long id);
+
+    //List<Transaction> findTransactionsByCategory(Category category);
 }
