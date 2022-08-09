a10315c255e377967b388219c2aae7e4ce72c43d
Volodymyr Romaniuk <volodyaromanyuk@gmail.com>
2022-08-09 18:44:59 +0300

    Added category dropdown menu elements auto creation

--- src/main/resources/static/js/transactions.js
+++ src/main/resources/static/js/transactions.js
@@ -40,7 +40,7 @@ function oneObjectInTable(obj) {
         "<td>" + obj.date + "</td>" +
         "<td>" + obj.description + "</td>" +
         "<td>" +
-        "<button type=\"button\" class=\"btn btn-info editButton\" data-bs-toggle=\"modal\" data-bs-target=\"#editModal\" data-bs-category-info='" +
+        "<button type=\"button\" class=\"btn btn-info editButton\" data-bs-toggle=\"modal\" data-bs-target=\"#editModal\" data-bs-transaction-info='" +
         JSON.stringify(obj) +
         "'" +
         ">Manage</button>" + "</td>" +
@@ -73,3 +73,29 @@ $(document).ready(function(){
         localStorage.clear();
     }
 });
+
+const requestCategoriesURL = baseURL + 'categories';
+const xhrCategories = new XMLHttpRequest();
+xhrCategories.open('GET', requestCategoriesURL);
+xhrCategories.onreadystatechange = function() {
+    if (xhrCategories.readyState !== 4 || xhrCategories.status !== 200) {
+        return;
+    }
+    //Set this json to constant
+    const response = xhrCategories.responseText;
+
+    //After that parse Json and write on site in table
+    const arrObjects = JSON.parse(response);
+
+    var finish = "";
+    for(let i = 0; i < arrObjects.length; i++) {
+        let obj = arrObjects[i];
+        finish += generateSelectOption(obj);
+    }
+    document.getElementById("transactionCategory").innerHTML = finish;
+}
+xhrCategories.send();
+
+function generateSelectOption(obj){
+    return "<option value = '" + JSON.stringify(obj) + "'>" + obj.name + "</option>"
+}
