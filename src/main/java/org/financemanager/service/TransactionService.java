ab5049c674adf6bfb4287687a50995825713738c
Volodymyr Romaniuk <volodyaromanyuk@gmail.com>
2022-08-18 22:19:47 +0300

    Removed unnecessary imports

--- src/main/java/org/financemanager/service/TransactionService.java
+++ src/main/java/org/financemanager/service/TransactionService.java
@@ -1,9 +1,6 @@
 package org.financemanager.service;
 
 import org.financemanager.entity.Transaction;
-import org.financemanager.repository.TransactionRepo;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
