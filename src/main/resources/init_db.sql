17ba9ccffef3524f7f1be4417268ce86c7fe406a
Volodymyr Romaniuk <volodyaromanyuk@gmail.com>
2022-08-18 22:15:08 +0300

    Removed database creation

--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -1,9 +1,3 @@
-create table if not exists categories(
-    id bigserial NOT NULL,
-    name text UNIQUE NOT NULL,
-    description text,
-    PRIMARY KEY (id)
-);
     select setval('categories_id_seq', (select max(id) from public.categories));
     insert into
     categories
@@ -32,24 +26,6 @@ create table if not exists categories(
     'categoryName5',
     'Description5'
 );
-
-create table if not exists public.transactions
-(
-    id bigserial NOT NULL,
-    category_id bigint NOT NULL,
-    operation_type text NOT NULL,
-    sum double precision NOT NULL,
-    date date NOT NULL,
-    description text,
-    PRIMARY KEY (id),
-    FOREIGN KEY (category_id) REFERENCES categories (id)
-    );
-
-ALTER TABLE IF EXISTS public.transactions
-    OWNER to postgres,
-    ADD CONSTRAINT check_operation_type
-    CHECK (operation_type = 'Spending' OR operation_type = 'Gain');
-
 
 select setval('transactions_id_seq', (select max(id) from public.transactions));
 insert into
