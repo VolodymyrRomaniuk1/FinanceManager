8faeb3b6362a1b51c065dde2c9f2e36ef1178d7f
Volodymyr Romaniuk <volodyaromanyuk@gmail.com>
2022-08-18 22:17:01 +0300

    Renamed some variables, added autoselection of category in <select>

--- src/main/resources/static/js/modalTransaction.js
+++ src/main/resources/static/js/modalTransaction.js
@@ -1,5 +1,5 @@
-const exampleModal = document.getElementById('editModal')
-exampleModal.addEventListener('show.bs.modal', event => {
+const modal = document.getElementById('editModal')
+modal.addEventListener('show.bs.modal', event => {
     // Button that triggered the modal
     const button = event.relatedTarget
     // Extract info from data-bs-* attributes
@@ -9,17 +9,22 @@ exampleModal.addEventListener('show.bs.modal', event => {
     // and then do the updating in a callback.
     //
     // Update the modal's content.
-    const modalTitle = exampleModal.querySelector('.modal-title')
-    const modalBodyId = exampleModal.querySelector('.modal-body #transactionId')
-    const modalBodyCategory = exampleModal.querySelector('.modal-body #transactionCategory')
-    const modalBodyOperationType = exampleModal.querySelector('.modal-body #transactionOperationType')
-    const modalBodySum = exampleModal.querySelector('.modal-body #transactionSum')
-    const modalBodyDate = exampleModal.querySelector('.modal-body #transactionDate')
-    const modalBodyDescription = exampleModal.querySelector('.modal-body #transactionDescription')
+    const modalTitle = modal.querySelector('.modal-title')
+    const modalBodyId = modal.querySelector('.modal-body #transactionId')
+    const modalBodyCategory = modal.querySelector('.modal-body #transactionCategory')
+    const modalBodyOperationType = modal.querySelector('.modal-body #transactionOperationType')
+    const modalBodySum = modal.querySelector('.modal-body #transactionSum')
+    const modalBodyDate = modal.querySelector('.modal-body #transactionDate')
+    const modalBodyDescription = modal.querySelector('.modal-body #transactionDescription')
 
     modalTitle.textContent = `Editing transaction, id: ${transaction.id}`;
     modalBodyId.value = transaction.id;
-    modalBodyCategory.value = transaction.category;
+    for (var i = 0; i < modalBodyCategory.options.length; i++){
+        modalBodyCategory.options[i].removeAttribute("selected");
+        if(JSON.parse(transactionInfo).category.id === JSON.parse(modalBodyCategory.options[i].value).id){
+            modalBodyCategory.options[i].setAttribute("selected", "selected");
+        }
+    }
     modalBodyOperationType.value = transaction.operationType;
     modalBodySum.value = transaction.sum;
     modalBodyDate.value = transaction.date;
@@ -34,20 +39,19 @@ function updateTransaction() {
     transaction.sum = document.getElementById('transactionSum').value;
     transaction.date = document.getElementById('transactionDate').value;
     transaction.description = document.getElementById('transactionDescription').value;
-    var categoryJSON = JSON.stringify(transaction);
+    var transactionJSON = JSON.stringify(transaction);
     console.log(transaction);
     const baseURL = 'http://localhost:8080/';
     const requestURL = baseURL + 'transactions/' + transaction.id;
     const xhr = new XMLHttpRequest();
     xhr.open('PUT', requestURL);
     xhr.setRequestHeader("Content-Type", "application/json");
-    xhr.send(categoryJSON);
+    xhr.send(transactionJSON);
     xhr.onreadystatechange = function () {
         if (xhr.readyState !== 4) {
             return;
         }
         localStorage.setItem("statusMessage", xhr.responseText)
-        console.log(xhr.responseText)
         window.location.reload();
     }
 }
