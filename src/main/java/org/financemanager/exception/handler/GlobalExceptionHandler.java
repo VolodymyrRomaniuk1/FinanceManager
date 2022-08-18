964c8c4992b0724d5c089def04cca11584263653
Volodymyr Romaniuk <volodyaromanyuk@gmail.com>
2022-08-18 22:14:17 +0300

    Added logging, added uncaught exception handler

--- src/main/java/org/financemanager/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/org/financemanager/exception/handler/GlobalExceptionHandler.java
@@ -4,33 +4,38 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.financemanager.exception.CategoryAlreadyExistsException;
 import org.financemanager.exception.NoSuchCategoryException;
-import org.financemanager.exception.NoSuchTransactionExeption;
+import org.financemanager.exception.NoSuchTransactionException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class GlobalExceptionHandler {
 
     public static final Logger logger = LogManager.getLogger(GlobalExceptionHandler.class);
 
     @ExceptionHandler(NoSuchCategoryException.class)
-    public ResponseEntity<String> NoSuchCategoryExceptionHandler(){
-        System.out.println(logger);
-        logger.error("NoSuchCategoryException");
+    public ResponseEntity<String> noSuchCategoryExceptionHandler(){
+        logger.error("NoSuchCategoryException thrown");
         return new ResponseEntity<>("No such category", HttpStatus.NOT_FOUND);
     }
 
-    @ExceptionHandler(NoSuchTransactionExeption.class)
-    public ResponseEntity<String> NoSuchTransactionExceptionHandler(){
-        logger.error("NoSuchTransactionException");
+    @ExceptionHandler(NoSuchTransactionException.class)
+    public ResponseEntity<String> noSuchTransactionExceptionHandler(){
+        logger.error("NoSuchTransactionException thrown");
         return new ResponseEntity<>("No such transaction", HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(CategoryAlreadyExistsException.class)
-    public ResponseEntity<String> CategoryAlreadyExistsException(){
-        logger.error("CategoryAlreadyExistsException");
+    public ResponseEntity<String> categoryAlreadyExistsException(){
+        logger.error("CategoryAlreadyExistsException thrown");
         return new ResponseEntity<>("Category already exists", HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> uncaughtExceptionHandler(Exception e){
+        logger.error("Thrown: " + e);
+        return new ResponseEntity<>("Error occured", HttpStatus.NOT_FOUND);
     }
 }
