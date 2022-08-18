544c4b28b3c6211fbb844d412ff562c11e4652e0
Volodymyr Romaniuk <volodyaromanyuk@gmail.com>
2022-08-18 22:12:29 +0300

    Removed unnecessary methods

--- src/main/java/org/financemanager/repository/CategoryRepo.java
+++ src/main/java/org/financemanager/repository/CategoryRepo.java
@@ -4,11 +4,8 @@ import org.financemanager.entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface CategoryRepo extends JpaRepository<Category, Long> {
+
     List<Category> findAll();
-    //Optional<Category> findById(Long id);
-    @Override
-    <S extends Category> S saveAndFlush(S s);
 }
