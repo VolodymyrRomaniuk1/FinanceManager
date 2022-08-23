84ae8991d510820200e055267e36595d57166d4d
Volodymyr Romaniuk <volodyaromanyuk@gmail.com>
2022-08-23 21:43:39 +0300

    Added more tests for TransactionController

--- src/test/java/org/financemanager/controller/CategoryControllerTest.java
+++ src/test/java/org/financemanager/controller/CategoryControllerTest.java
@@ -5,11 +5,7 @@ import com.fasterxml.jackson.databind.ObjectWriter;
 import org.financemanager.entity.Category;
 import org.financemanager.exception.NoSuchCategoryException;
 import org.financemanager.exception.handler.GlobalExceptionHandler;
-import org.financemanager.repository.CategoryRepo;
-import org.financemanager.repository.TransactionRepo;
-import org.financemanager.service.CategoryService;
 import org.financemanager.service.impl.CategoryServiceImpl;
-import org.json.JSONArray;
 import org.json.JSONObject;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -18,31 +14,24 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.boot.test.json.JacksonTester;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MockMvcBuilder;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.BDDMockito.given;
-import static org.mockito.BDDMockito.willReturn;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.mockito.BDDMockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 
 
 @ExtendWith(MockitoExtension.class)
 public class CategoryControllerTest {
 
     private MockMvc mvc;
-
-    @Mock
-    private CategoryRepo categoryRepo;
 
     @Mock
     private CategoryServiceImpl categoryService;
@@ -77,15 +66,11 @@ public class CategoryControllerTest {
                 .andReturn()
                 .getResponse();
 
-        System.out.println(response.getStatus());
-        System.out.println(response.getContentAsString());
-        System.out.println(new JSONObject(ow.writeValueAsString(category)));
         assertThat(response.getStatus()).isEqualTo(HttpStatus.OK.value());
         assertThat(new ObjectMapper().readValue(response.getContentAsString(), Category.class)).isEqualTo(category);
-
     }
 
-    @DisplayName("JUnit test for getById method (negative scenario)")
+    @DisplayName("JUnit test for getById method (no such category)")
     @Test
     public void givenNoSuchCategoryExceptionThrown_whenGetById_thenReturn404() throws Exception{
         given(categoryService.findById(1L)).willThrow(NoSuchCategoryException.class);
@@ -95,11 +80,9 @@ public class CategoryControllerTest {
                 .andReturn()
                 .getResponse();
 
-        System.out.println(response.getStatus());
-        System.out.println(response.getContentAsString());
         assertThat(response.getStatus()).isEqualTo(HttpStatus.NOT_FOUND.value());
     }
-//"?name=\"Category Name 1212\"&description=\"Description asd asd 123\""//String.valueOf(new JSONObject(ow.writeValueAsString(category)))
+
     @DisplayName("JUnit test for saveCategory method")
     @Test
     public void whenSaveCategory_thenReturn200() throws Exception{
@@ -114,7 +97,7 @@ public class CategoryControllerTest {
         assertThat(response.getStatus()).isEqualTo(HttpStatus.CREATED.value());
     }
 
-    @DisplayName("JUnit test for saveCategory method (negative scenario)")
+    @DisplayName("JUnit test for saveCategory method (with bad arguments)")
     @Test
     public void whenSaveCategoryWithErrors_thenReturn404() throws Exception{
         MockHttpServletResponse response = mvc.perform(
@@ -122,6 +105,64 @@ public class CategoryControllerTest {
                                 .param("name", "C")
                                 .param("description", "Description asd asd 123")
                 )
+                .andReturn()
+                .getResponse();
+
+        assertThat(response.getStatus()).isEqualTo(HttpStatus.NOT_FOUND.value());
+    }
+
+    @DisplayName("JUnit test for updateCategory method")
+    @Test
+    public void whenUpdateCategory_thenReturn200() throws Exception{
+        category.setName("Updated Category Name");
+        category.setDescription("Updated Category Description");
+
+        MockHttpServletResponse response = mvc.perform(
+                        put("/categories/1").contentType(MediaType.APPLICATION_JSON)
+                                .content(String.valueOf(new JSONObject(ow.writeValueAsString(category))))
+                )
+                .andReturn()
+                .getResponse();
+
+        assertThat(response.getStatus()).isEqualTo(HttpStatus.OK.value());
+    }
+
+    @DisplayName("JUnit test for updateCategory method (with bad arguments)")
+    @Test
+    public void whenUpdateCategoryWithErrors_thenReturn404() throws Exception{
+        category.setName("U");
+        category.setDescription("Updated Category Description");
+
+        MockHttpServletResponse response = mvc.perform(
+                        put("/categories/1").contentType(MediaType.APPLICATION_JSON)
+                                .content(String.valueOf(new JSONObject(ow.writeValueAsString(category))))
+                )
+                .andReturn()
+                .getResponse();
+
+        assertThat(response.getStatus()).isEqualTo(HttpStatus.NOT_FOUND.value());
+    }
+
+    @DisplayName("JUnit test for deleteCategory method")
+    @Test
+    public void whenDeleteCategory_thenReturn200() throws Exception{
+        willDoNothing().given(categoryService).delete(category.getId());
+
+        MockHttpServletResponse response = mvc.perform(delete("/categories/1")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andReturn()
+                .getResponse();
+
+        assertThat(response.getStatus()).isEqualTo(HttpStatus.OK.value());
+    }
+
+    @DisplayName("JUnit test for deleteCategory method (no such category)")
+    @Test
+    public void whenDeleteCategory_thenReturn404() throws Exception{
+        willThrow(NoSuchCategoryException.class).given(categoryService).delete(category.getId());
+
+        MockHttpServletResponse response = mvc.perform(delete("/categories/1")
+                        .contentType(MediaType.APPLICATION_JSON))
                 .andReturn()
                 .getResponse();
 
