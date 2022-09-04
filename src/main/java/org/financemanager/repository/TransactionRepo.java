3df54deb8cea98ca2e8ce8b546ae8fc20ad83db9
Volodymyr Romaniuk <volodyaromanyuk@gmail.com>
2022-09-04 20:43:18 +0300

    Added methods to find by date and operation type

--- src/main/java/org/financemanager/repository/TransactionRepo.java
+++ src/main/java/org/financemanager/repository/TransactionRepo.java
@@ -1,12 +1,19 @@
 package org.financemanager.repository;
 
+import org.financemanager.entity.Category;
 import org.financemanager.entity.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.sql.Date;
 import java.util.List;
 
 public interface TransactionRepo extends JpaRepository<Transaction, Long> {
+    @Query("select t from Transaction t order by t.id asc")
     List<Transaction> findAll();
 
+    List<Transaction> findAllByDateBetween(Date dateStart, Date dateEnd);
+
+    List<Transaction> findAllByDateBetweenAndOperationType(Date dateStart, Date dateEnd, String operationType);
     //List<Transaction> findTransactionsByCategory(Category category);
 }
