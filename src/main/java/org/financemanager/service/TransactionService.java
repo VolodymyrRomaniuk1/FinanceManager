4426c298a460ef1eb8cdc55dc5b5976d42768f17
Volodymyr Romaniuk <volodyaromanyuk@gmail.com>
2022-09-04 20:43:28 +0300

    Added methods to find by date and operation type

--- src/main/java/org/financemanager/service/TransactionService.java
+++ src/main/java/org/financemanager/service/TransactionService.java
@@ -1,12 +1,18 @@
 package org.financemanager.service;
 
+import org.financemanager.entity.Category;
 import org.financemanager.entity.Transaction;
 
+import java.sql.Date;
 import java.util.List;
 import java.util.Optional;
 
 public interface TransactionService {
     List<Transaction> findAll();
+
+    List<Transaction> findAllByDateBetween(Date dateStart, Date dateEnd);
+
+    List<Transaction> findAllByDateBetweenAndOperationType(Date dateStart, Date dateEnd, String operationType);
 
     Optional<Transaction> findById(Long id);
 
