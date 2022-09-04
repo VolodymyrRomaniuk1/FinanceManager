918f2421fa899e17ba661c66b9ff27e337f3e3c2
Volodymyr Romaniuk <volodyaromanyuk@gmail.com>
2022-09-04 20:39:43 +0300

    Added sorting by id

--- src/main/java/org/financemanager/repository/CategoryRepo.java
+++ src/main/java/org/financemanager/repository/CategoryRepo.java
@@ -2,10 +2,11 @@ package org.financemanager.repository;
 
 import org.financemanager.entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface CategoryRepo extends JpaRepository<Category, Long> {
-
+    @Query("select c from Category c order by c.id asc")
     List<Category> findAll();
 }
